cb51c5730140645a2ea98a58aa0285d0991c1d34
Emiliano1823 <106696081+Emiliano1823@users.noreply.github.com>
2022-06-01 12:33:36 -0500

    Initial commit

---
+++ js/music-list.js
@@ -0,0 +1,56 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Andamos de Regreso",
+    artist: "Julión Alvarez",
+    img: "AlbumJulion",
+    src: "music-1"
+  },
+  {
+    name: "Aquí nada va a cambiar",
+    artist: "Julión Alvarez",
+    img: "AlbumJulion",
+    src: "music-2"
+  },
+  {
+    name: "Como este cabrón",
+    artist: "Julión Alvarez",
+    img: "AlbumJulion",
+    src: "music-3"
+  },
+  {
+    name: "Pongámonos de Acuerdo",
+    artist: "Julión Alvarez",
+    img: "AlbumJulion",
+    src: "music-4"
+  },
+  {
+    name: "Ojos Verdes",
+    artist: "Julión Alvarez",
+    img: "AlbumJulion",
+    src: "music-5"
+  },
+  {
+    name: "Más te Recuerdo",
+    artist: "Julión Alvarez",
+    img: "AlbumJulion",
+    src: "music-6"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
